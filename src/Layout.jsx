eb43e17363b031f6ca1cd4668298e1fb1c33b026
sabeerbikba <sabeerbikba02@gmail.com>
2024-03-12 17:47:09 +0530

    fix

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -6,18 +6,18 @@ export default function Layout() {
         <>
             <nav>
                 <ul className="sidnav">
-                    <li><NavLink className={classs} to="SearchEngine">SearchEngines</NavLink></li>
-                    <li><NavLink className={classs} to="LiveHtml">LiveHtml</NavLink></li>
-                    <li><NavLink className={classs} to="MetaTagsGenrator">MetaTagsGenrator</NavLink></li>
-                    <li><NavLink className={classs} to="GrapesJSEditor">GrapesJSEditor</NavLink></li>
-                    <li><NavLink className={classs} to="UnitConverter">UnitConverter</NavLink></li>
-                    <li><NavLink className={classs} to="CharacterAndWordCounter">WordCounter</NavLink></li>
-                    <li><NavLink className={classs} to="ColorConverter">ColorConverter</NavLink></li>
-                    <li><NavLink className={classs} to="Browser-Ready-CSS">Browser-Ready-CSS</NavLink></li>
-                    <li><NavLink className={classs} to="StringConverter">StringConverter</NavLink></li>
-                    <li><NavLink className={classs} to="QrCodeGenerator"> QRCodeGenerator</NavLink></li>
-                    <li><NavLink className={classs} to="HashGenerator">HashGenerator</NavLink></li>
-                    <li><NavLink className={classs} to="Websites">Websites</NavLink></li>
+                    <li><NavLink className={classs} to="/SearchEngine">SearchEngines</NavLink></li>
+                    <li><NavLink className={classs} to="/LiveHtml">LiveHtml</NavLink></li>
+                    <li><NavLink className={classs} to="/MetaTagsGenrator">MetaTagsGenrator</NavLink></li>
+                    <li><NavLink className={classs} to="/GrapesJSEditor">GrapesJSEditor</NavLink></li>
+                    <li><NavLink className={classs} to="/UnitConverter">UnitConverter</NavLink></li>
+                    <li><NavLink className={classs} to="/CharacterAndWordCounter">WordCounter</NavLink></li>
+                    <li><NavLink className={classs} to="/ColorConverter">ColorConverter</NavLink></li>
+                    <li><NavLink className={classs} to="/Browser-Ready-CSS">Browser-Ready-CSS</NavLink></li>
+                    <li><NavLink className={classs} to="/StringConverter">StringConverter</NavLink></li>
+                    <li><NavLink className={classs} to="/QrCodeGenerator"> QRCodeGenerator</NavLink></li>
+                    <li><NavLink className={classs} to="/HashGenerator">HashGenerator</NavLink></li>
+                    <li><NavLink className={classs} to="/Websites">Websites</NavLink></li>
                 </ul>
             </nav>
             <main className="main">
