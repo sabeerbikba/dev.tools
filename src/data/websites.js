9900ff589e17490959111cad5e8cc347487e6acc
sabeerbikba <sabeerbikba02@gmail.com>
2024-03-13 19:01:57 +0530

    trying  uploading images in diffrent server

--- src/data/websites.js
+++ src/data/websites.js
@@ -1,17 +1,163 @@
-export const baseUrl = '/src/public/'
+// export const baseUrl = '/src/public/'
+// export const websites = {
+//     "Image Optimization": [
+//         {
+//             heading: "Images Compression",
+//             text: "Compress WebP, PNG and JPEG images to optimize web page loding times",
+//             link: "https://tinypng.com/",
+//             img: 'image5.png'
+//         },
+//         {
+//             heading: "Sprites Generator",
+//             text: "Tool for creating CSS sprite sheets (Better to use when all image same size). CSS sprites are a technique used to combine multiple images into a single image file to reduce the number of server requests,",
+//             link: "https://www.toptal.com/developers/css/sprite-generator",
+//             img: 'image6.jpg'
+//         }
+//     ],
+//     "File Conversion": [
+//         {
+//             heading: "Convert anything",
+//             text: "website offering online file conversion services. Users can convert various types of files, such as documents, images, videos, audio, and more, between different formats",
+//             link: "https://convertio.co/",
+//             img: 'convert.io.png'
+//         }
+//     ],
+//     "Website Optimization": [
+//         {
+//             heading: "PageSpeed Insights",
+//             text: "tool by Google that analyzes the performance of web pages on both mobile and desktop devices. It provides suggestions and optimizations to improve page loading speed and user experience, helping developers enhance their websites' performance and usability.",
+//             link: "https://pagespeed.web.dev/",
+//             img: 'pageinsights.png',
+//         }
+//     ],
+//     "Color Tools": [
+//         {
+//             heading: "BrandColors",
+//             text: "Color used by company to in their website",
+//             link: "http://brandcolors.net/",
+//             img: 'image2.jpg'
+//         },
+//         {
+//             heading: "CSS Color Gradient",
+//             text: "tool that genrate css code for backgound with gradient effect",
+//             link: "https://cssgradient.io/",
+//             img: 'image12.png'
+//         },
+//         {
+//             heading: "SVG Backgrounds",
+//             text: "Provides customizable SVG backgrou// need to add image nd with various patterns and colors",
+//             link: "https://svgbackgrounds.com/",
+//             img: 'image14.png'
+//         },
+//         {
+//             heading: "Color Palettes",
+//             text: "A collection of color palettes for inspiration.",
+//             link: "https://colorhunt.co/",
+//             img: 'image4.png'
+//         },
+//         {
+//             heading: "Material Palette",
+//             text: "A tool for creating Material Desingn color palettes",
+//             link: "https://materialpalette.com/",
+//             img: 'image7.png'
+//         },
+//         {
+//             heading: "Color Palette Generator",
+//             text: "A tool for Genrating Color Palettes for creting websites",
+//             link: "https://coolors.co/",
+//             img: 'image11.png'
+//         },
+//         {
+//             heading: "Adobe Color Wheel",
+//             text: "Allow you to create, explore and save color schemes",
+//             link: "https://color.adobe.com/create",
+//             img: 'image3.png',
+//         }
+//     ],
+//     "Font and Icon": [
+//         {
+//             heading: "Fonts Suggest",
+//             text: "Generate font combinations with deep learning",
+//             link: "https://fontjoy.com/",
+//             img: 'image1.gif'
+//         },
+//         {
+//             heading: "Icons",
+//             text: "free and easy intigration icons (change icon)",
+//             link: "https://heroicons.com/",
+//             img: 'image8.png'
+//         },
+//         {
+//             heading: "Bootstrap Icons",
+//             text: "icons by bootstrap",
+//             link: "https://icons.getbootstrap.com/",
+//             img: 'image9.png'
+//         }
+//     ],
+//     "Diagramming, Visualization and SVG": [
+//         {
+//             heading: 'Figma',
+//             text: 'Figma is a web-based design tool for creating SVGs, prototypes, and diagrams. It offers intuitive features and collaborative capabilities, making it popular among designers and teams for efficient project creation and sharing',
+//             link: 'https://www.figma.com/',
+//             img: 'figma.png'
+//         },
+//         {
+//             heading: "Draw.io",
+//             text: "Draw.io is a online diagramming website that allows users to create diagrams, flowcharts and various other visual representations (vscode extension avilable)",
+//             link: "https://app.diagrams.net/",
+//             img: 'image10.png'
+//         },
+//     ],
+//     "Regular Expression": [
+//         {
+//             heading: "regexr.com",
+//             text: ' website that provides a comprehensive online tool for creating, testing, debugging and learning regular expressions for javascript',
+//             link: "https://regexr.com/",
+//             img: 'regexr.com.png'
+//         },
+//         {
+//             heading: "regex-genrator",
+//             text: "The website generates regular expressions tailored to specific patterns, aiding in efficient string matching and manipulation for developers. It's a handy tool for simplifying complex pattern matching tasks in web development.",
+//             link: "https://regex-generator.olafneumann.org/",
+//             img: 'regex-gen.png'
+//         }
+//     ],
+//     "Simmilar Websites": [
+//         {
+//             heading: 'Devtoolbox.co',
+//             text: 'Tools diff viewer, JSON validator, unix time converter, regex matcher, character counter, and more',
+//             link: 'https://www.devtoolbox.co/',
+//             img: 'devtoolbox_logo.png',
+//         },
+//         {
+//             heading: 'Transform.tools',
+//             text: 'Utilities for web development tasks, including formatting, validation, encoding/decoding, and manipulation of data formats such as JSON, XML, CSS, JavaScript, and HTML',
+//             link: 'https://transform.tools/',
+//             img: 'transform.png',
+//         },
+//         {
+//             heading: 'CodeBeautify.org',
+//             text: 'online tools for developers, including code formatting and beautification, JSON/XML/HTML validation and formatting, text manipulation, data conversion, encryption/decryption, and many other utilities to assist in coding and development tasks',
+//             link: 'https://codebeautify.org/',
+//             img: 'codebeautify.png',
+//         },
+//     ]
+// };
+
+export const baseUrl = 'https://i.imgur.com/' // images :: https://imgur.com/a/5o8otTb
 export const websites = {
     "Image Optimization": [
         {
             heading: "Images Compression",
             text: "Compress WebP, PNG and JPEG images to optimize web page loding times",
             link: "https://tinypng.com/",
-            img: 'image5.webp'
+            img: 'LhHW2jY.png'
         },
         {
             heading: "Sprites Generator",
             text: "Tool for creating CSS sprite sheets (Better to use when all image same size). CSS sprites are a technique used to combine multiple images into a single image file to reduce the number of server requests,",
             link: "https://www.toptal.com/developers/css/sprite-generator",
-            img: 'image6.jpg'
+            img: 'VsXBXNa.jpeg'
         }
     ],
     "File Conversion": [
@@ -19,7 +165,7 @@ export const websites = {
             heading: "Convert anything",
             text: "website offering online file conversion services. Users can convert various types of files, such as documents, images, videos, audio, and more, between different formats",
             link: "https://convertio.co/",
-            img: 'convert.io.png'
+            img: 'squO8WU.png'
         }
     ],
     "Website Optimization": [
@@ -27,7 +173,7 @@ export const websites = {
             heading: "PageSpeed Insights",
             text: "tool by Google that analyzes the performance of web pages on both mobile and desktop devices. It provides suggestions and optimizations to improve page loading speed and user experience, helping developers enhance their websites' performance and usability.",
             link: "https://pagespeed.web.dev/",
-            img: 'pageinsights.png',
+            img: 'Ok6e1Dp.png',
         }
     ],
     "Color Tools": [
@@ -35,43 +181,43 @@ export const websites = {
             heading: "BrandColors",
             text: "Color used by company to in their website",
             link: "http://brandcolors.net/",
-            img: 'image2.jpg'
+            img: '4T4wVt8.jpeg'
         },
         {
             heading: "CSS Color Gradient",
             text: "tool that genrate css code for backgound with gradient effect",
             link: "https://cssgradient.io/",
-            img: 'image12.png'
+            img: 'AJGHdkC.png'
         },
         {
             heading: "SVG Backgrounds",
             text: "Provides customizable SVG backgrou// need to add image nd with various patterns and colors",
             link: "https://svgbackgrounds.com/",
-            img: 'image14.png'
+            img: 'xvsEtfr.png'
         },
         {
             heading: "Color Palettes",
             text: "A collection of color palettes for inspiration.",
             link: "https://colorhunt.co/",
-            img: 'image4.png'
+            img: 'LM2CA0k.png'
         },
         {
             heading: "Material Palette",
             text: "A tool for creating Material Desingn color palettes",
             link: "https://materialpalette.com/",
-            img: 'image7.png'
+            img: '5h3OuW9.png'
         },
         {
             heading: "Color Palette Generator",
             text: "A tool for Genrating Color Palettes for creting websites",
             link: "https://coolors.co/",
-            img: 'image11.png'
+            img: '99RYKBX.png'
         },
         {
             heading: "Adobe Color Wheel",
             text: "Allow you to create, explore and save color schemes",
             link: "https://color.adobe.com/create",
-            img: 'image3.png',
+            img: 'Xgeu4xJ.png',
         }
     ],
     "Font and Icon": [
@@ -79,19 +225,19 @@ export const websites = {
             heading: "Fonts Suggest",
             text: "Generate font combinations with deep learning",
             link: "https://fontjoy.com/",
-            img: 'image1.gif'
+            img: 'UhnWHSG.gif'
         },
         {
             heading: "Icons",
             text: "free and easy intigration icons (change icon)",
             link: "https://heroicons.com/",
-            img: 'image8.png'
+            img: 'KlhL4xf.png'
         },
         {
             heading: "Bootstrap Icons",
             text: "icons by bootstrap",
             link: "https://icons.getbootstrap.com/",
-            img: 'image9.png'
+            img: 'tldYPnL.png'
         }
     ],
     "Diagramming, Visualization and SVG": [
@@ -99,13 +245,13 @@ export const websites = {
             heading: 'Figma',
             text: 'Figma is a web-based design tool for creating SVGs, prototypes, and diagrams. It offers intuitive features and collaborative capabilities, making it popular among designers and teams for efficient project creation and sharing',
             link: 'https://www.figma.com/',
-            img: 'figma.png'
+            img: 'sCrOWn1.png'
         },
         {
             heading: "Draw.io",
             text: "Draw.io is a online diagramming website that allows users to create diagrams, flowcharts and various other visual representations (vscode extension avilable)",
             link: "https://app.diagrams.net/",
-            img: 'image10.png'
+            img: 'TPgJt9i.png'
         },
     ],
     "Regular Expression": [
@@ -113,13 +259,13 @@ export const websites = {
             heading: "regexr.com",
             text: ' website that provides a comprehensive online tool for creating, testing, debugging and learning regular expressions for javascript',
             link: "https://regexr.com/",
-            img: 'regexr.com.png'
+            img: '2I8niYo.png'
         },
         {
             heading: "regex-genrator",
             text: "The website generates regular expressions tailored to specific patterns, aiding in efficient string matching and manipulation for developers. It's a handy tool for simplifying complex pattern matching tasks in web development.",
             link: "https://regex-generator.olafneumann.org/",
-            img: 'regex-gen.ico'
+            img: 'SCykHur.png'
         }
     ],
     "Simmilar Websites": [
@@ -127,19 +273,19 @@ export const websites = {
             heading: 'Devtoolbox.co',
             text: 'Tools diff viewer, JSON validator, unix time converter, regex matcher, character counter, and more',
             link: 'https://www.devtoolbox.co/',
-            img: 'devtoolbox_logo.png',
+            img: 'DOkCZfS.png',
         },
         {
             heading: 'Transform.tools',
             text: 'Utilities for web development tasks, including formatting, validation, encoding/decoding, and manipulation of data formats such as JSON, XML, CSS, JavaScript, and HTML',
             link: 'https://transform.tools/',
-            img: 'transform.png',
+            img: 'HbQi4lB.png',
         },
         {
             heading: 'CodeBeautify.org',
             text: 'online tools for developers, including code formatting and beautification, JSON/XML/HTML validation and formatting, text manipulation, data conversion, encryption/decryption, and many other utilities to assist in coding and development tasks',
             link: 'https://codebeautify.org/',
-            img: 'codebeautify.png',
+            img: '8nqxQY1.png',
         },
     ]
 };
