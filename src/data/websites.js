cc4e963f2926cec88c6200e3ab2dd14deb8257d6
sabeerbikba <sabeerbikba02@gmail.com>
2024-03-13 16:35:30 +0530

    removed unused images and those left images moved in public  folder reason: not rendering in production

--- src/data/websites.js
+++ src/data/websites.js
@@ -1,87 +1,4 @@
-const cards = [{
-    heading: 'Images Compression',
-    text: 'Compress WebP, PNG and JPEG images to optimize web page loding times',
-    link: 'https://tinypng.com/',
-    imgClassNumber: '1'
-}, {
-    heading: 'Sprites Genrator',
-    text: 'Tool for creating CSS sprite sheets (Better to use when all image same size). CSS sprites are a technique used to combine multiple images into a single image file to reduce the number of server requests,',
-    link: 'https://www.toptal.com/developers/css/sprite-generator',
-    imgClassNumber: '2'
-}, {
-    heading: 'Convert anything',
-    text: 'website offering online file conversion services. Users can convert various types of files, such as documents, images, videos, audio, and more, between different formats',
-    link: 'https://convertio.co/',
-    imgClassNumber: '3'
-}, {
-    heading: 'BrandColors',
-    text: 'Color used by company to in their website',
-    link: 'http://brandcolors.net/',
-    imgClassNumber: '4'
-}, {
-    heading: 'CSS color Gradiet',
-    text: 'tool that genrate css code for backgound with gradient effect',
-    link: 'https://cssgradient.io/',
-    imgClassNumber: '5'
-}, {
-    heading: 'SVG Backgrounds',
-    text: 'Provides customizable SVG background with various patterns and colors',
-    link: 'https://svgbackgrounds.com/',
-    imgClassNumber: '6'
-}, {
-    heading: 'Color Palettes',
-    text: 'A collection of color palettes for inspiration.',
-    link: 'https://colorhunt.co/',
-    imgClassNumber: '7'
-}, {
-    heading: 'Material Palette',
-    text: 'A tool for creating Material Desingn color palettes',
-    link: 'https://materialpalette.com/',
-    imgClassNumber: '8'
-}, {
-    heading: 'Color Pelette Genrator',
-    text: 'A tool for Genrating Color Palettes for creting websites',
-    link: 'https://coolors.co/',
-    imgClassNumber: '9'
-}, {
-    heading: 'Adobe Color Wheel',
-    text: 'Allow you to create, explore and save color schemes',
-    link: 'https://color.adobe.com/create',
-    imgClassNumber: '10'
-}, {
-    heading: 'Fonts Suggest',
-    text: 'Generate font combinations with deep learning',
-    link: 'https://fontjoy.com/',
-    imgClassNumber: '11'
-}, {
-    heading: 'Icons',
-    text: 'free and easy intigration icons (change icon)',
-    link: 'https://heroicons.com/',
-    imgClassNumber: '12'
-}, {
-    heading: 'Draw.io',
-    text: 'Draw.io is a online diagramming website that allows users to create diagrams, flowcharts and various other visual representations (vscode extension avilable)',
-    link: 'https://app.diagrams.net/',
-    imgClassNumber: '13'
-}, {
-    heading: 'Bootstrap icons',
-    text: 'icons by bootstrap',
-    link: 'https://icons.getbootstrap.com/',
-    imgClassNumber: '14'
-}, {
-    heading: 'regExGen1',
-    text: 'text',
-    link: 'https://regexr.com/',
-    imgClassNumber: '15'
-}, {
-    heading: 'regExGen2',
-    texg: 'lorem ipsum',
-    link: 'https://regex-generator.olafneumann.org/',
-    imgClassNumber: '16'
-}]
-export default cards;
-
-export const baseUrl = './src/assets/website/old/'
+export const baseUrl = './src/public/website/'
 export const websites = {
     "Image Optimization": [
         {
@@ -110,7 +27,7 @@ export const websites = {
             heading: "PageSpeed Insights",
             text: "tool by Google that analyzes the performance of web pages on both mobile and desktop devices. It provides suggestions and optimizations to improve page loading speed and user experience, helping developers enhance their websites' performance and usability.",
             link: "https://pagespeed.web.dev/",
-            img: 'pageinsights.png',// need to add image 
+            img: 'pageinsights.png',
         }
     ],
     "Color Tools": [
@@ -154,7 +71,7 @@ export const websites = {
             heading: "Adobe Color Wheel",
             text: "Allow you to create, explore and save color schemes",
             link: "https://color.adobe.com/create",
-            img: 'image3.png'// need to add image 
+            img: 'image3.png',
         }
     ],
     "Font and Icon": [
@@ -225,4 +142,4 @@ export const websites = {
             img: 'codebeautify.png',
         },
     ]
-}
+};
