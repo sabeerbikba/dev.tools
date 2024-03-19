0b68a6fa1b2d99521079113e16c7677b0be913d5
sabeerbikba <sabeerbikba02@gmail.com>
2024-03-19 16:31:44 +0530

    removed style.css from import

--- src/index.jsx
+++ src/index.jsx
@@ -2,9 +2,7 @@ import { StrictMode } from 'react';
 import ReactDOM from 'react-dom/client';
 
 import App from './App.jsx';
-// import './styles.css';
 import './output.css';
-import './styles.css';
 
 // document.body.innerHTML = '<div id="root"></div>'; //remove all other body elements inside html body
 ReactDOM.createRoot(document.getElementById('root')).render(
