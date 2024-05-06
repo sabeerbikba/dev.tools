962a623b2f6266956849afc5a955d713a622ee27
sabeerbikba <sabeerbikba02@gmail.com>
2024-05-06 13:54:48 +0530

    06/05/24

--- README.md
+++ README.md
@@ -3,7 +3,7 @@
 
 This project is a collection of useful developer tools built with React.
 
-## Tools Included:
+## Tools Included
 
 - All in One SearchEngines
 - CSS Autoprefixer
@@ -13,8 +13,8 @@ This project is a collection of useful developer tools built with React.
 - Hash Generator
 - Live HTML Preview
 - Meta Tags Generator
+- NameCase Converter
 - QR Code Generator
-- Collection of Search Engines with Single Input
 - Unit Converter (e.g., px to em)
 - Website Links for Developers
 
@@ -33,15 +33,17 @@ npm install
 # 4. Run the development server
 npm run dev
 ```
-##### It will automatically open a web browser. If it doesn't open, please open your web browser and navigate to http://localhost:3004.
 
+##### It will automatically open a web browser. If it doesn't open, please open your web browser and navigate to <http://localhost:3004>
 
-## Feel Free to Contribute!
+## Feel Free to Contribute
+
 We welcome contributions from everyone! Whether you're a developer, designer, or just passionate about the project, there are many ways to contribute.
 
 If you're interested in contributing, take a look at our [task.md](task.md) file for a list of tasks and milestones. Feel free to pick up any task you're interested in or suggest new ones!
 
 ### **License**
+
 The project is licensed under the [MIT License](LICENSE). You can check out the license for more details.
 
 **Happy contributing!**
--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
+import { SpeedInsights } from '@vercel/speed-insights/react';
+import { Analytics } from '@vercel/analytics/react';
+
 import { useEffect, lazy } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { ToastContainer, toast } from 'react-toastify';
 import { isMobile } from 'react-device-detect';
-import { SpeedInsights } from '@vercel/speed-insights/react';
-import { Analytics } from '@vercel/analytics/react';
 import 'react-toastify/dist/ReactToastify.css';
 
 import Layout from "./Layout";
@@ -25,42 +26,42 @@ import Websites from "./pages/Websites.jsx";
 // import Test from "./pages/testing/Test.jsx" // Testing purpose
 
 export default function App() {
-  useEffect(() => {
-    if (isMobile) {
-      toast.warn('Warning: This website is best viewed on a larger screen.', {
-        position: toast.POSITION.BOTTOM_CENTER,
-        autoClose: 5000,
-      });
-    }
-  }, []);
+   useEffect(() => {
+      if (isMobile) {
+         toast.warn('Warning: This website is best viewed on a larger screen.', {
+            position: toast.POSITION.BOTTOM_CENTER,
+            autoClose: 5000,
+         });
+      }
+   }, []);
 
-  return (
-    <>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Layout />}>
-            <Route index element={<SearchEngine />} />
-            <Route path="SearchEngine" element={<SearchEngine />} />
-            <Route path="LiveHtml" element={<SuspenseWithFallback><LiveHtml /></SuspenseWithFallback>} />
-            <Route path="MetaTagsGenrator" element={<SuspenseWithFallback><MetaTagsGenrator /></SuspenseWithFallback>} />
-            <Route path="GrapesjsEditor" element={<SuspenseWithFallback><GrapesJSEditor /></SuspenseWithFallback>} />
-            <Route path="UnitConverter" element={<UnitConverter />} />
-            <Route path="CharacterAndWordCounter" element={<CharacterAndWordCounter />} />
-            <Route path="ColorConverter" element={<SuspenseWithFallback><ColorConverter /></SuspenseWithFallback>} />
-            <Route path="Browser-Ready-CSS" element={<SuspenseWithFallback><AutoprefixerTool /></SuspenseWithFallback>} />
-            <Route path="StringConverter" element={<StringConverter />} />
-            <Route path="QrCodeGenerator" element={<SuspenseWithFallback><QrCodeGenerator /></SuspenseWithFallback>} />
-            <Route path="HashGenerator" element={<SuspenseWithFallback><HashGenerator /></SuspenseWithFallback>} />
-            <Route path="Websites" element={<Websites />} />
-            {/* <Route path="Test" element={<Test />} /> */}
-            <Route path="*" element={<NoPage />} />
-          </Route>
-        </Routes>
-      </BrowserRouter>
-      <ToastContainer />
-      {/* vercel */}
-      <SpeedInsights />
-      <Analytics />
-    </>
-  );
+   return (
+      <>
+         <BrowserRouter>
+            <Routes>
+               <Route path="/" element={<Layout />}>
+                  <Route index element={<SearchEngine />} />
+                  <Route path="SearchEngine" element={<SearchEngine />} />
+                  <Route path="LiveHtml" element={<SuspenseWithFallback><LiveHtml /></SuspenseWithFallback>} />
+                  <Route path="MetaTagsGenrator" element={<SuspenseWithFallback><MetaTagsGenrator /></SuspenseWithFallback>} />
+                  <Route path="GrapesjsEditor" element={<SuspenseWithFallback><GrapesJSEditor /></SuspenseWithFallback>} />
+                  <Route path="UnitConverter" element={<UnitConverter />} />
+                  <Route path="CharacterAndWordCounter" element={<CharacterAndWordCounter />} />
+                  <Route path="ColorConverter" element={<SuspenseWithFallback><ColorConverter /></SuspenseWithFallback>} />
+                  <Route path="Browser-Ready-CSS" element={<SuspenseWithFallback><AutoprefixerTool /></SuspenseWithFallback>} />
+                  <Route path="StringConverter" element={<StringConverter />} />
+                  <Route path="QrCodeGenerator" element={<SuspenseWithFallback><QrCodeGenerator /></SuspenseWithFallback>} />
+                  <Route path="HashGenerator" element={<SuspenseWithFallback><HashGenerator /></SuspenseWithFallback>} />
+                  <Route path="Websites" element={<Websites />} />
+                  {/* <Route path="Test" element={<Test />} /> */}
+                  <Route path="*" element={<NoPage />} />
+               </Route>
+            </Routes>
+         </BrowserRouter>
+         <ToastContainer />
+         {/* vercel */}
+         <SpeedInsights />
+         <Analytics />
+      </>
+   );
 }
