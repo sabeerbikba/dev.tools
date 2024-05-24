b6686ae0c8cff76dc992a1890c3e0b13eed78318
sabeerbikba <sabeerbikba02@gmail.com>
2024-05-24 13:11:24 +0530

    Added format code in LiveHtml component.

--- src/pages/LiveHtml.jsx
+++ src/pages/LiveHtml.jsx
@@ -1,28 +1,27 @@
-import MonacoEditor from '@monaco-editor/react';
 import { useState } from 'react';
+import MonacoEditor from '@monaco-editor/react';
+import beautify from 'js-beautify';
 
-import useLocalStorage from "../hooks/useLocalStorage";
+import useLocalStorageState from "../hooks/useLocalStorageState";
 import CopyBtn from '../common/CopyBtn';
 
+const initCode = `<h2>Hello world!</h2>`
+
 export default function LiveHtml() {
+   const [code, setCode] = useLocalStorageState('liveHtmlCode2', initCode);
    const [copyBtnDisabled, setCopyBtnDisabled] = useState(false);
-   const [code, setCode] = useLocalStorage('liveHtmlCode', '');
 
-   function handleClear() {
-      setCode('');
-   }
 
-   const tailwindcss = {
-      btn: "rounded-md bg-indigo-500 px-3.5 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-500 h-12"
+   const formatCode = () => {
+      const beautifiedCode = beautify.html(code);
+      setCode(beautifiedCode);
    }
 
    const styles = {
       main: { width: '100%', height: '100%', display: 'flex', minWidth: '1620px' },
       div50: { width: '50%', height: '100%', display: 'flex', flexDirection: 'column', background: '#808080cc' },
       btnDiv: { display: 'flex', justifyContent: 'space-around', height: '5%' },
-      btn: { width: '35%', height: '92%' },
-      'h95%': { height: '95%' },
-      iframeDiv: { width: '50%', height: '100%' },
+      btn: { width: '30%', height: '92%', color: 'white', borderRadius: '5px', backgroundColor: `${!code.trim() ? '#4446a6' : 'rgb(99, 102, 241)'}` }, 'h95%': { height: '95%' }, livepreviewDiv: { width: '50%', height: '100%' },
    }
 
    return (
@@ -30,10 +29,15 @@ export default function LiveHtml() {
          <div style={styles.div50}>
             <div style={styles.btnDiv}>
                <button
-                  style={{ ...styles.btn, backgroundColor: `${!code.trim() ? '#4446a6' : ''}` }}
-                  className={tailwindcss.btn}
-                  onClick={handleClear}
+                  style={styles.btn}
+                  onClick={() => setCode('')}
+                  disabled={code.trim() === ''}
                >Clear</button>
+               <button
+                  style={styles.btn}
+                  onClick={formatCode}
+                  disabled={code.trim() === ''}
+               >Format</button>
                <CopyBtn
                   copyText={code}
                   styles={styles.btn}
@@ -51,7 +55,7 @@ export default function LiveHtml() {
                />
             </div>
          </div>
-         <div style={styles.iframeDiv}>
+         <div style={styles.livepreviewDiv}>
             <iframe
                srcDoc={`<html><head></head><body style="color: white;">${code}</body></html>`}
                title="Live Preview"
