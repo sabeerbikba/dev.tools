025dc273b4e73466d985e77459825ada678c82ab
sabeerbikba <sabeerbikba02@gmail.com>
2024-09-12 21:20:27 +0530

    Add '@' alias to Vite configuration for src directory

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
+import path from 'path';
 
 export default defineConfig({
    plugins: [react()],
@@ -8,5 +9,10 @@ export default defineConfig({
       port: 3004,
       open: true,
       https: false,
-   }
+   },
+   resolve: {
+      alias: {
+         '@': path.resolve(__dirname, './src'),
+      },
+   },
 });
