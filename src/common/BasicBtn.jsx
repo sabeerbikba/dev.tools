9065efff563bd661f587ffe1bdb4c60adaa16edd
sabeerbikba <sabeerbikba02@gmail.com>
2024-09-16 16:35:58 +0530

    chore: add comment explaining use of ! in Tailwind class for btn

--- src/common/BasicBtn.jsx
+++ src/common/BasicBtn.jsx
@@ -1,30 +1,30 @@
 import PropTypes from 'prop-types';
 
 const BasicBtn = ({
-   onClick,
-   btnText,
-   classNames = '',
-   styles = {},
-   btnDisabled,
+  onClick,
+  btnText,
+  classNames = '',
+  styles = {},
+  btnDisabled,
 }) => {
-   // Nothing fancy just added styles
+  // Nothing fancy just added styles
 
-   return (
-      <button
-         onClick={onClick}
-         className={classNames + ' btn'}
-         style={styles}
-         disabled={btnDisabled}
-      >{btnText}</button>
-   );
+  return (
+    <button
+      onClick={onClick}
+        className={classNames + ' btn'} // Some Tailwind class names require the !important modifier due to the btn class
+      style={styles}
+      disabled={btnDisabled}
+    >{btnText}</button>
+  );
 };
 
 BasicBtn.propTypes = {
-   onClick: PropTypes.func.isRequired,
-   btnText: PropTypes.string.isRequired,
-   className: PropTypes.string,
-   styles: PropTypes.object,
-   btnDisabled: PropTypes.bool,
+  onClick: PropTypes.func.isRequired,
+  btnText: PropTypes.string.isRequired,
+  className: PropTypes.string,
+  styles: PropTypes.object,
+  btnDisabled: PropTypes.bool,
 }
 
 export default BasicBtn;
