6d263b82b93da719d24d7c7185ec928388c1084a
sabeerbikba <sabeerbikba02@gmail.com>
2024-09-16 16:37:41 +0530

    refactor: update code structure and add height prop

--- src/common/ToolBoxLayout.jsx
+++ src/common/ToolBoxLayout.jsx
@@ -1,13 +1,22 @@
 import PropTypes from "prop-types";
 
-const ToolBoxLayout = ({ children }) => {
-   return (<div className="flex gap-4 p-4 w-full h-full text-white"
-      style={{ minWidth: '1620px', height: '100vh', overflow: 'hidden' }}
-   >{children}</div>);
+const ToolBoxLayout = ({
+   children,
+   height = '100vh',
+}) => {
+   return (
+      <div
+         className="flex gap-4 p-4 w-full h-full text-white"
+         style={{ minWidth: '1620px', height: height, overflow: 'hidden' }}
+      >
+         {children}
+      </div>
+   );
 }
 
 ToolBoxLayout.propTypes = {
    children: PropTypes.node.isRequired,
+   height: PropTypes.string,
 }
 
 export default ToolBoxLayout;
